479046d2f47607d1c5e10868732f8997deb08243
Milo <zeroeclipse@live.com>
2021-12-14 00:37:40 -0500

    Adding functions and components.

--- src/context/shopContext.js
+++ src/context/shopContext.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
-import Client from 'shopify-buy';
+import Client from 'shopify-buy'
+
+const ShopContext = React.createContext()
 
 const client = Client.buildClient({
     domain: process.env.REACT_APP_SHOPIFY_DOMAIN,
@@ -7,10 +9,55 @@ const client = Client.buildClient({
   });
 
 class ShopProvider extends Component {
+    state = {
+        product: {},
+        products: [],
+        checkout: {},
+        isCartOpen: false,
+        isMenuOpen: false
+    }
+
+    createCheckout = async () => {
+
+    }
+
+    fetchCheckout = async () => {
+
+    }
+
+    removeLineItem = async (lineItemIdsToRemove) => {
+
+    }
+    fetchAllProducts = async () => {
+
+    }
+
+    fetchProductWithHandle = async (handle) => {
+
+    }
+
+    closeCart = () => {
+
+    }
+
+    openCart = () => {
+
+    }
+
+    closeMenu = () => {
+
+    }
+
+    openMenu = () => {
+
+    }
+
     render() {
         return (
             <div>
-                
+                <ShopContext.Provider>
+                    {}
+                </ShopContext.Provider>
             </div>
         )
     }
