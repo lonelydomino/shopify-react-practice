0f888a83f1a568f7a26ac27f2b01ced14e67c8a3
Milo <zeroeclipse@live.com>
2021-12-19 21:51:03 -0500

    adding chakra ui.

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals'
 import ShopProvider from './context/shopContext'
+import { ChakraProvider } from '@chakra-ui/react'
 
 ReactDOM.render(
 
   <React.StrictMode>
+    <ChakraProvider>
     <ShopProvider>
       <App />
     </ShopProvider>
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
