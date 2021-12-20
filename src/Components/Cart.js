4f145072d7469f07cbf28922b5b82dc6d459d6e0
Milo <zeroeclipse@live.com>
2021-12-20 18:14:15 -0500

    Adding Drawer imports from chakraUI

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,6 +1,16 @@
 import React, { useContext } from 'react'
 import { ShopContext } from '../context/shopContext'
 
+import {
+    Drawer,
+    DrawerBody,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerOverlay,
+    DrawerContent,
+    DrawerCloseButton 
+} from '@chakra-ui/react'
+
 const Cart = () => {
     return (
         <div>
