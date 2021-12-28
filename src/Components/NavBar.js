79666c3d7a1a626cef214762f1dfddb4705c512d
Milo <zeroeclipse@live.com>
2021-12-27 18:02:47 -0800

    Making logo direct to home.

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react'
 import { Flex, Icon, Image, Text } from '@chakra-ui/react'
 import { ShopContext } from '../context/shopContext'
 import { MdMenu, MdShoppingBasket } from 'react-icons/md'
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
 
@@ -10,7 +11,7 @@ const NavBar = () => {
     return (
         <Flex backgroundColor="#FFA8E2" flexDirection="row" justifyContent="space-between" p="2rem" >
             <Icon fill="white" cursor="pointer" as={MdMenu} w={30} h={30}></Icon>
-            <Image cursor="pointer" src="https://cdn.shopify.com/s/files/1/0472/5705/9496/files/Logologo_1.svg?v=1610055540" w={100} h={100}/>
+            <Link to="/"><Image cursor="pointer" src="https://cdn.shopify.com/s/files/1/0472/5705/9496/files/Logologo_1.svg?v=1610055540" w={100} h={100}/></Link>
             <Icon cursor="pointer" fill="white" as={MdShoppingBasket} w={30} h={30} onClick={()=> openCart()}></Icon>
         </Flex>
     )
