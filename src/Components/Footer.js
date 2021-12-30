b7f85182715acd42c8b4f5ead53b1f00d76e913e
Milo <zeroeclipse@live.com>
2021-12-29 17:29:58 -0800

    Finishing footer.

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,8 +4,25 @@ import { Grid, Box, Text, Image, VStack } from '@chakra-ui/react'
 
 const Footer = () => {
     return (
-        <Box>
-            
+        <Box backgroundColor="#FFA8E2">
+            <Grid templateColumns={["repeat(1, 1fr)", "repeat(3, 1fr)"]} color={"white"} fontWeight={"bold"}>
+                <Image src="https://cdn.shopify.com/s/files/1/0472/5705/9496/files/Bath_Bomb_-_Product-5_-_trans_1.png?=1610123549" />
+                <VStack p="2rem">
+                    <Link to="/">The Green Blast</Link>
+                    <Link to="/">The Blue Berry</Link>
+\                    <Link to="/">The Yellow Mellowo</Link>
+                </VStack>
+                <VStack p="2rem">
+                    <Link to="/">About Us</Link>
+                    <Link to="/">Learn More</Link>
+\                    <Link to="/">Sustainability</Link>
+                </VStack>
+            </Grid>
+            <Box>
+                <Text textAlign={"center"} color="white" w="100%" borderTop="1px solid white" p="1rem">
+                    © Michael Martinez
+                </Text>
+            </Box>
         </Box>
     )
 }
