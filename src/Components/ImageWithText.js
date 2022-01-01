4795799fe32753e96fbfd3964c653126d80c8af8
Milo <zeroeclipse@live.com>
2021-12-31 20:25:35 -0800

    Adding Image With Text component.

---
+++ src/Components/ImageWithText.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Box, Flex, Button, Text, Image, Heading } from '@chakra-ui/react'
+
+const ImageWithText = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ImageWithText
