63ef6062f15dc7ef5ccae2011d13a447b0743917
Milo <zeroeclipse@live.com>
2021-12-31 20:38:24 -0800

    Finishing Image With Text component.

--- src/Components/ImageWithText.js
+++ src/Components/ImageWithText.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import { Box, Flex, Button, Text, Image, Heading } from '@chakra-ui/react'
 
-const ImageWithText = () => {
+const ImageWithText = ({ reverse, image, heading, text }) => {
+
+    const reverseSection = reverse ? 'row-reverse' : 'row'
+
     return (
-        <div>
-            
-        </div>
+        <Box>
+            <Flex flexDir={['column', reverseSection]} w="100%">
+                <Image src={image} objectFit="cover" w={["100%", "50%"]} />
+                <Flex w={["100%", "50%"]} flexDir="column" justifyContent="center" alignItems="center" p="2rem">
+                    <Heading p="2rem">
+                        {heading && heading}
+                    </Heading>
+                    <Text p="2rem">{text && text}</Text>
+                    <Button p="2rem" w="10rem" backgroundColor="#FF38BD" color="white" _hover={{opacity: '70%'}}>Buy Now</Button>
+                </Flex>
+            </Flex>
+        </Box>
     )
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import { ShopContext } from '../context/shopContext'
 import { Link } from 'react-router-dom'
 import { Box, Text, Grid, Image } from '@chakra-ui/react'
 import Hero from '../components/Hero'
+import ImageWithText from '../components/ImageWithText'
 
 const Home = () => {
     const {fetchAllProducts, products} = useContext(ShopContext)
@@ -27,7 +28,9 @@ const Home = () => {
                     </Link>
                 ))}
             </Grid>
-           
+           <ImageWithText text="stuff stuff stuff stuff" reverse image="https://cdn.shopify.com/s/files/1/0472/5705/9496/files/premium-bath-bombs.jpg?v=1610066758" heading="Heading"/>
+           <ImageWithText text="stuff stuff stuff stuff"  image="https://cdn.shopify.com/s/files/1/0472/5705/9496/files/premium-bath-bombs.jpg?v=1610066758" heading="Heading"/>
+           <ImageWithText text="stuff stuff stuff stuff" reverse image="https://cdn.shopify.com/s/files/1/0472/5705/9496/files/premium-bath-bombs.jpg?v=1610066758" heading="Heading"/>
         </Box>
     )
 }
