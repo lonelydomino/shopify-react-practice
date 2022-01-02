aca061a1e4184fda8b787835522e53b7e6cd325c
Milo <zeroeclipse@live.com>
2022-01-02 12:03:57 -0800

    Adding animation to hero text.

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -6,7 +6,7 @@ const Hero = () => {
         <Box backgroundColor="#FFA8E2" w="100%" h="70vh" position="relative">
             <Image src="https://cdn.shopify.com/s/files/1/0472/5705/9496/files/Bath_Bomb_-_Product-4_-_nobg_1.png?v=1610055851.jpg" h="100%" m="auto" objectFit="contain"
             objectPosition={["top", "center"]}/>
-            <Text position="absolute" bottom="20%" w="100%" textAlign="center" color="white" fontWeight="bold" fontSize="4rem">
+            <Text className="tracking-in-expand" position="absolute" bottom="20%" w="100%" textAlign="center" color="white" fontWeight="bold" fontSize="4rem">
                 Introducing Bath Bombs
             </Text>
             <Center>
